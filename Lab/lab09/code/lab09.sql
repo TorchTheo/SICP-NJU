dc7b298142a1f6e800bed0555e6912b7b3a8b37c
TorchTheo <62044597+TorchTheo@users.noreply.github.com>
2020-12-26 10:00:43 +0800

    12.25

---
+++ Lab/lab09/code/lab09.sql
@@ -0,0 +1,42 @@
+.read lab09data.sql
+
+CREATE TABLE smallest_int AS
+  SELECT time, smallest FROM students WHERE smallest > 2 ORDER BY smallest LIMIT 20;
+
+CREATE TABLE matchmaker AS
+  SELECT a.pet, a.song, a.color, b.color FROM students AS a, students AS b WHERE a.pet = b.pet AND a.song = b.song AND a.time < b.time;
+
+CREATE TABLE parents AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+
+CREATE TABLE dogs AS
+  SELECT "abraham" AS name, "long" AS fur, 26 AS height UNION
+  SELECT "barack"         , "short"      , 52           UNION
+  SELECT "clinton"        , "long"       , 47           UNION
+  SELECT "delano"         , "long"       , 46           UNION
+  SELECT "eisenhower"     , "short"      , 35           UNION
+  SELECT "fillmore"       , "curly"      , 32           UNION
+  SELECT "grover"         , "short"      , 28           UNION
+  SELECT "herbert"        , "curly"      , 31;
+
+CREATE TABLE sizes AS
+  SELECT "toy" AS size, 24 AS min, 28 AS max UNION
+  SELECT "mini"       , 28       , 35        UNION
+  SELECT "medium"     , 35       , 45        UNION
+  SELECT "standard"   , 45       , 60;
+
+-- Add your INSERT INTOs here
+CREATE TABLE smallest_int_having AS
+  SELECT time, smallest FROM students GROUP BY smallest HAVING  COUNT(*) = 1;
+
+CREATE TABLE sicp20favpets AS
+  SELECT pet, COUNT(*) FROM students GROUP BY pet ORDER BY COUNT(*) DESC LIMIT 10;
+
+CREATE TABLE sicp20dog AS
+  SELECT pet, COUNT(*) FROM students WHERE pet = "dog" GROUP BY pet;
